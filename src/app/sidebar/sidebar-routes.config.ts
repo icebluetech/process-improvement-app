8c7d7e34f6aa8e0d0b3fdab017b6c8810d1a89cc
Christopher Cheshire <Christopher.Cheshire@denvergov.org>
2017-08-20 23:02:53 -0600

    icons and wording

--- src/app/sidebar/sidebar-routes.config.ts
+++ src/app/sidebar/sidebar-routes.config.ts
@@ -8,17 +8,18 @@ export const ROUTES: RouteInfo[] = [
     { path: 'docs', title: 'Docs', menuType: MenuType.LEFT, icon:'pe-7s-science' },//4
     { path: 'notifications', title: 'Notifications', menuType: MenuType.LEFT, icon:'pe-7s-bell' },//5
     { path: 'settings', title: 'settings', menuType: MenuType.LEFT, icon:'pe-7s-tools' },//6
-    { path: 'rootcause', title: 'Gap Analysis', menuType: MenuType.LEFT, icon:'fa fa-code-fork' },//7
+    { path: 'rootcause', title: 'Gap Analysis', menuType: MenuType.LEFT, icon:'fa fa-line-chart' },//7
     { path: 'standardwork', title: 'Standard Work', menuType: MenuType.LEFT, icon:'fa fa-file-text-o' },//8
     { path: 'training', title: 'Training', menuType: MenuType.LEFT, icon:'pe-7s-study' },//9
     { path: 'brainstorm', title: 'Brainstorming', menuType: MenuType.LEFT, icon:'fa fa-lightbulb-o' },//10
-    { path: 'experiment', title: 'Experiments', menuType: MenuType.LEFT, icon:'fa fa-flask' },//11
-    { path: 'result', title: 'Results', menuType: MenuType.LEFT, icon:'fa fa-gavel' },//12
-    { path: 'why', title: 'Why Change', menuType: MenuType.LEFT, icon:'fa fa-question' },//13
-    { path: 'current', title: 'Current State', menuType: MenuType.LEFT, icon:'fa fa-area-chart' },//14
-    { path: 'future', title: 'Future State', menuType: MenuType.LEFT, icon:'fa fa-line-chart' },//15
+    { path: 'experiment', title: 'Solution Approach', menuType: MenuType.LEFT, icon:'fa fa-flask' },//11
+    { path: 'confirmed', title: 'Confirmed State', menuType: MenuType.LEFT, icon:'fa fa-gavel' },//12
+    { path: 'reason', title: 'Reason for Action', menuType: MenuType.LEFT, icon:'fa fa-question' },//13
+    { path: 'initial', title: 'Initial State', menuType: MenuType.LEFT, icon:'fa fa-bicycle' },//14
+    { path: 'target', title: 'Target State', menuType: MenuType.LEFT, icon:'fa fa-rocket' },//15
     { path: 'gap', title: 'Gap Analaysis', menuType: MenuType.LEFT, icon:'fa fa-cogs' },//16
-    { path: 'action', title: 'Action Plan', menuType: MenuType.LEFT, icon:'fa fa-list-alt' },//17
-    { path: 'insights', title: 'Insights', menuType: MenuType.LEFT, icon:'fa fa-eye' }//18
+    { path: 'action', title: 'Completion Plan', menuType: MenuType.LEFT, icon:'fa fa-list-alt' },//17
+    { path: 'insights', title: 'Insights', menuType: MenuType.LEFT, icon:'fa fa-eye' },//18
+    { path: 'inno', title: 'Innovations', menuType: MenuType.LEFT, icon:'fa fa-bolt' }//18
     
 ];
